6bbd1476827f6302641a3194dabeec62051512ed
Cody Clarke <clarke.cody@gmail.com>
2021-05-21 15:16:48 -0700

    forms

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { TextPropTypes } from 'react-native';
-import { FlatList } from 'react-native';
+import { View, FlatList } from 'react-native';
 import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent'
 
 const mapStateToProps = state => {
    return {
@@ -28,6 +29,16 @@ class Directory extends Component{
                     onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
                     imageSrc={{ uri: baseUrl+ item.image}}
                 />
+            );
+        }
+        if(this.props.campsites.isLoading) {
+            return <Loading />
+        }
+        if(this.props.campsites.errMess) {
+            return(
+                <View>
+                    <Text>{this.props.campsites.errMess}</Text>
+                </View>
             );
         }
         return(
