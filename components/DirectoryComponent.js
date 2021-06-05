3aa632cf6bcc3d2b66f8a31f62caedc52f021592
Cody Clarke <clarke.cody@gmail.com>
2021-06-05 09:57:58 -0700

    workshop .1

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import { TextPropTypes } from "react-native";
 import { View, FlatList } from "react-native";
 import { Tile } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
 import Loading from "./LoadingComponent";
 import * as Animatable from "react-native-animatable";
+
 
 const mapStateToProps = (state) => {
   return {
--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -7,6 +7,7 @@ import * as ImagePicker from "expo-image-picker";
 import * as Permissions from "expo-permissions";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { baseUrl } from "../shared/baseUrl";
+import * as ImageManipulator from "expo-image-manipulator";
 
 class LoginTab extends Component {
   constructor(props) {
@@ -159,9 +160,21 @@ class RegisterTab extends Component {
       });
       if (!capturedImage.cancelled) {
         console.log(capturedImage);
-        this.setState({ imageUrl: capturedImage.uri });
+        this.processImage(capturedImage.uri);
       }
     }
+  };
+
+  processImage = async (imgUri) => {
+    const processedImage = await ImageManipulator.manipulateAsync(
+      imgUri,
+      [{ resize: { width: "400px" } }],
+      {
+        format: "png",
+      }
+    );
+    console.log(processedImage);
+    this.setState({ imageUrl: processedImage.uri });
   };
 
   handleRegister() {
@@ -180,6 +193,7 @@ class RegisterTab extends Component {
       );
     }
   }
+
   render() {
     return (
       <ScrollView>
